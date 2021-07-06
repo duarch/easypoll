fe944f88aa59d130e0c8b0c2620ca29ce24b0879
duarch <duxiaoan@gmail.com>
2021-07-05 22:17:46 -0300

    until clipboard button

---
+++ forms.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type ContactDetails struct {
+	Email   string
+	Subject string
+	Message string
+}
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("forms.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+		}
+
+		details := ContactDetails{
+			Email: r.FormValue("question"),
+
+			Subject: r.FormValue("opt1"),
+			Message: r.FormValue("opt2"),
+		}
+
+		// do something with details
+		_ = details
+
+		tmpl.Execute(w, struct{ Success bool }{true})
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
